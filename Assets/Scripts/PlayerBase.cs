46ff06ab4f11caeab6a9b7de8e1ef35452e13deb
Christopher Mooney <ckmooney@usc.edu>
2016-03-26 15:29:55 -0700

    Created player base script

---
+++ Assets/Scripts/PlayerBase.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBase : Building {
+
+    PlayerBase(Vector3 position) : base(position)
+    {
+        pylonRange = 3;
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
