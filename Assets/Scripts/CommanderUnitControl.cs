ce4223af404c2c564cdec4b94f1ab43e03b6ab6d
Albert <ayue@usc.edu>
2016-03-26 17:02:56 -0700

    Abilities UI implemented. Panel pops up when units are selected

--- Assets/Scripts/CommanderUnitControl.cs
+++ Assets/Scripts/CommanderUnitControl.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class CommanderUnitControl : MonoBehaviour {
 
     private ArrayList selectedUnits = new ArrayList();
+    private GameObject unitAbilitiesPanel;
 
 	// Use this for initialization
 	void Start () {
-	    
+	    unitAbilitiesPanel = GameObject.Find("Canvas/UnitAbilitiesPanel");
+        if(unitAbilitiesPanel != null)
+            unitAbilitiesPanel.SetActive(false);
 	}
 	
     void OnDrawGizmos()
@@ -21,74 +24,99 @@ public class CommanderUnitControl : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         Vector3 mousePosWorld = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+
         if (Input.GetMouseButtonDown(0))
         {
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, mousePosWorld - transform.position, out hit))
+            LeftClick(mousePosWorld);
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            RightClick(mousePosWorld);
+        }
+
+	}
+
+    void LeftClick(Vector3 mousePosWorld)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, mousePosWorld - transform.position, out hit))
+        {
+            Debug.DrawLine(transform.position, hit.point, Color.green);
+            Unit clickedUnit = hit.transform.GetComponent<Unit>();
+            if (clickedUnit != null)
             {
-                Debug.DrawLine(transform.position, hit.point, Color.green);
-                Unit clickedUnit = hit.transform.GetComponent<Unit>();
-                if (clickedUnit != null)
+                if (selectedUnits.Contains(clickedUnit))//This unit is already selected
                 {
-                    if (selectedUnits.Contains(clickedUnit))//This unit is already selected
+                    if (!Input.GetKey(KeyCode.LeftShift))//If multiselect key is not being held, make this the only selected unit
                     {
-                        if (!Input.GetKey(KeyCode.LeftShift))//If multiselect key is not being held, make this the only selected unit
-                        {
-                            Clear();
-                            selectedUnits.Add(clickedUnit);
-                            clickedUnit.Select(true);       //change unit color for selection
-                        }
-                        else {//Otherwise, deselect this unit
-                            selectedUnits.Remove(clickedUnit);
-                            clickedUnit.Select(false);      //change unit color for selection
-                        }
-                    }
-                    else//Otherwise add it as a selected unit
-                    {
-                        if (!Input.GetKey(KeyCode.LeftShift)) //If multiselect key is not being held, deselect all other units
-                        {
-                            Clear();
-                        }
+                        Clear();
                         selectedUnits.Add(clickedUnit);
                         clickedUnit.Select(true);       //change unit color for selection
                     }
+                    else
+                    {//Otherwise, deselect this unit
+                        selectedUnits.Remove(clickedUnit);
+                        clickedUnit.Select(false);      //change unit color for selection
+                    }
                 }
-                else {
-                    Debug.DrawLine(transform.position, mousePosWorld, Color.red);
-                    if (!Input.GetKey(KeyCode.LeftShift)) //If multiselect key is not being held, deselect all units
+                else//Otherwise add it as a selected unit
+                {
+                    if (!Input.GetKey(KeyCode.LeftShift)) //If multiselect key is not being held, deselect all other units
                     {
                         Clear();
                     }
+                    selectedUnits.Add(clickedUnit);
+                    clickedUnit.Select(true);       //change unit color for selection
                 }
             }
-            else {
-                Debug.DrawLine(transform.position, mousePosWorld, Color.grey);
+            else
+            {
+                Debug.DrawLine(transform.position, mousePosWorld, Color.red);
                 if (!Input.GetKey(KeyCode.LeftShift)) //If multiselect key is not being held, deselect all units
                 {
                     Clear();
                 }
             }
         }
-        if (Input.GetMouseButtonDown(1))
+        else
         {
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, mousePosWorld - transform.position, out hit))
+            Debug.DrawLine(transform.position, mousePosWorld, Color.grey);
+            if (!Input.GetKey(KeyCode.LeftShift)) //If multiselect key is not being held, deselect all units
             {
-                Debug.DrawLine(transform.position, hit.point, Color.magenta);
-                Unit clickedUnit = hit.transform.GetComponent<Unit>();
-                if (clickedUnit != null)    //We clicked on another unit, so we should contextually decide what action to do
+                Clear();
+            }
+        }
+
+        // Display UI Menu for unit abilities. Currently just displays menu if unit is selected. Hides if no units are selected
+        if (selectedUnits.Count > 0) 
+        {
+            unitAbilitiesPanel.SetActive(true);
+        }
+        else if(selectedUnits.Count == 0)
+        {
+            unitAbilitiesPanel.SetActive(false);
+        }
+    }
+
+    void RightClick(Vector3 mousePosWorld)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, mousePosWorld - transform.position, out hit))
+        {
+            Debug.DrawLine(transform.position, hit.point, Color.magenta);
+            Unit clickedUnit = hit.transform.GetComponent<Unit>();
+            if (clickedUnit != null)    //We clicked on another unit, so we should contextually decide what action to do
+            {
+            }
+            else //We clicked on the ground, so tell the units to move there
+            {
+                foreach (Unit unit in selectedUnits)
                 {
-                }
-                else //We clicked on the ground, so tell the units to move there
-                {
-                    foreach (Unit unit in selectedUnits)
-                    {
-                        unit.MoveTo(hit.point);
-                    }
+                    unit.MoveTo(hit.point);
                 }
             }
         }
-	}
+    }
 
     void Clear()
     {
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -26,31 +26,7 @@ public class Unit : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //if (isMoving)
-        //{
-        //    Vector3 toEnd = destination - transform.position;
 
-        //    //Calculate pathfinding
-        //    //NavMeshPath path = new NavMeshPath();
-        //    //navmeshagent.CalculatePath(destination, path);
-
-        //    //for(int i=1; i<path.corners.Length; i++)
-        //    //{
-        //    //    if (i % 2 == 0)
-        //    //        Debug.DrawLine(path.corners[i], path.corners[i - 1], Color.blue);
-        //    //    else
-        //    //        Debug.DrawLine(path.corners[i], path.corners[i - 1], Color.cyan);
-        //    //}
-
-        //    //Vector3 toNextWaypoint = path.corners[1] - transform.position;
-
-        //    //Vector3 moveForce = moveSpeed * toNextWaypoint.normalized;
-        //    //rigidbody.AddForce(moveForce, ForceMode.VelocityChange);
-        //    //if (toEnd.magnitude <= stopDistance)
-        //    //    isMoving = false;
-
-        //    navmeshagent.SetDestination(toEnd);
-        //}
 	}
 
     public void MoveTo(Vector3 destination)
