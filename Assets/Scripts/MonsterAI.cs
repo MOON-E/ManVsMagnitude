e9e20aae48545e5326205e6b2db74fd5e66220c5
Alec Schule <aschule@usc.edu>
2016-03-27 19:05:45 -0700

    Monster AI Toggle Added in Alec's scene

--- Assets/Scripts/MonsterAI.cs
+++ Assets/Scripts/MonsterAI.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class MonsterAI : MonoBehaviour {
     public float min_time = 0.2f; // how often monster acts
 
     public CommandBuffer buffer;
+    public Toggle toggle;
+
     private float timer;
 
 	// Use this for initialization
@@ -14,6 +17,7 @@ public class MonsterAI : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+            if (!toggle.isOn) return;
             if (Time.time > timer + min_time) { // trigger monster action
                 int action = (int)(Random.value * 100) % 4;
                 buffer.Input(action);
