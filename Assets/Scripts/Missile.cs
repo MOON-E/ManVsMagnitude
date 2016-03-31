5ff44003051fc490502c0264a6a083015f29c333
Vincent Bertoni <vbertoni@usc.edu>
2016-03-30 22:42:00 -0700

    building cooldowns chat command recognition changed

--- Assets/Scripts/Missile.cs
+++ Assets/Scripts/Missile.cs
@@ -4,13 +4,30 @@ using System.Collections;
 public class Missile : Building {
 
     public float chargeTime;
-    public bool ready;
+    public bool ready = false;
+	private float currCharge=0;
 
     void Start()
     {
         base.Start();
         ready = true;
     }
+
+	void Update() {
+		base.Update ();
+		if (mBuildState == BuildState.COMPLETED) {
+			isAFactory = true;
+			ready = true;
+		}
+		if (!ready) {
+			currCharge += Time.deltaTime;
+			if (currCharge >= chargeTime) {
+				GetComponent<MeshRenderer>().material.color = Color.green;
+				ready = true;
+				currCharge = 0;
+			}
+		}
+	}
 
     public void launch()
     {
@@ -19,5 +36,7 @@ public class Missile : Building {
         Camera.main.GetComponent<CommanderBuildingControl>().MissileLaunch();
 
         Debug.Log("missile click");
+		GetComponent<MeshRenderer>().material.color = Color.red;
+		ready = false;
     }
 }
