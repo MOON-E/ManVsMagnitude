5800d2a730456fa612089242eb0c94f848b0664a
Christopher Mooney <ckmooney@usc.edu>
2016-03-30 21:44:48 -0700

    Controls screen, Music asset added

---
+++ Assets/Scripts/ControlsButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ControlsButton : MonoBehaviour {
+
+    public RawImage controls;
+
+    public void Toggle()
+    {
+        Debug.Log("toggle");
+        if (controls.enabled) controls.enabled = false;
+        else controls.enabled = true;
+    }
+}
