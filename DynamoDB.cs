a9e128934328359708b681f3d976072010794cf5
Sophia Spackova <sophiaspackova@gmail.com>
2016-09-03 19:36:32 -0700

    Not entirely sure if this was all the files..
    ...for the database which is still in need of some more work.

---
+++ DynamoDB.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using Amazon.DynamoDBv2;
+using Amazon.CognitoIdentity;
+using Amazon.Runtime;
+using Amazon;
+
+public class DynamoDB : MonoBehaviour {
+	public string CognitoPoolRegion = RegionEndpoint.USWest1.SystemName;
+	public string DynamoRegion = RegionEndpoint.USWest1.SystemName;
+	public string IdentityPoolId = "";
+
+	private RegionEndpoint _CognitoPoolRegion
+	{
+		get { return RegionEndpoint.GetBySystemName(CognitoPoolRegion); }
+	}
+
+	private RegionEndpoint _DynamoRegion
+	{
+		get { return RegionEndpoint.GetBySystemName(DynamoRegion); }
+	}
+
+	private static IAmazonDynamoDB _ddbClient;
+	private AWSCredentials _credentials;
+
+	private AWSCredentials Credentials
+	{
+		get
+		{
+			if (_credentials == null)
+				_credentials = new CognitoAWSCredentials(IdentityPoolId, _CognitoPoolRegion);
+			return _credentials;
+		}
+	}
+
+	protected IAmazonDynamoDB Client
+	{
+		get
+		{
+			if (_ddbClient == null)
+			{
+				_ddbClient = new AmazonDynamoDBClient(Credentials, _DynamoRegion);
+			}
+
+			return _ddbClient;
+		}
+	}
+}
---
+++ Table.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using Amazon.DynamoDBv2.DataModel;
+using System.Collections.Generic;
+using Amazon.DynamoDBv2;
+using UnityEngine.UI;
+
+public class Table : DynamoDB {
+
+	private IAmazonDynamoDB _client;
+	private DynamoDBContext _context;
+	private Text txt;
+
+	private DynamoDBContext Context
+	{
+		get
+		{
+			if(_context == null)
+				_context = new DynamoDBContext(_client);
+
+			return _context;
+		}
+	}
+
+	void Awake() 
+	{
+		_client = Client;
+	}
+
+	void CreateUser(string Username)
+	{
+		user newestuser = new user {
+			username = Username,
+			NumTimesDragonGreen = 0
+		};
+
+		Context.SaveAsync (newestuser);
+	}
+
+
+	[DynamoDBTable("UserData")]
+	public class user
+	{
+		[DynamoDBHashKey]
+		public string username { get; set; }
+		[DynamoDBProperty]
+		public int NumTimesDragonGreen { get; set; }
+
+	}
+}
